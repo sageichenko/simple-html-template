2613801c043b89d7d74bf3a84e99caaf8b660da2
user <user@iMac-user.local>
2018-11-27 10:57:52 +0300

    version 1.0

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,4 @@
 // Your code here...
-function changeColor (color) {
-	document.getElementById('tshirt-img').src = 'img/tshirts/tshirt_' + color + '.jpg';
+function changeColor(color) {
+    document.getElementById('tshirt-img').src = 'img/tshirts/tshirt_' + color + '.jpg';
 }
